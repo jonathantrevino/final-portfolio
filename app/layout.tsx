4ae6df5961262a7679a2334c43c5c6c13d02b20b
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-07-27 00:01:29 -0500

    adding favicon, and manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,10 @@ export const metadata: Metadata = {
         url: "/logo-dark.png",
       },
     ],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
   },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
